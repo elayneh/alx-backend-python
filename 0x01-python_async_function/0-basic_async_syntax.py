6b87309633a89dfe077003df1f3ad3774bc3c7ec
elayneh <betenatefera@gmail.com>
2022-06-27 16:28:54 +0300

    variable annotation is added

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
-    rand = random.uniform(0, max_delay)
+async def wait_random(max_delay: int = 10) -> float:
+    rand: float = random.uniform(0, max_delay)
     await asyncio.sleep(rand)
     return rand
