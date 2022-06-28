eb304182502a037e11bae90e89da8b57f13638f5
elayneh <betenatefera@gmail.com>
2022-06-28 21:43:08 +0300

    documentation added

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,6 +6,11 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
+    """_summary_
+
+    Yields:
+        Generator[float, None, None]: _description_
+    """
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
