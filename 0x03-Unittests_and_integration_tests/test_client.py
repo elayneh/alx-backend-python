fa6aada295009ba3655150fc76ae7be7d233c04e
elayneh <betenatefera@gmail.com>
2022-07-18 15:47:51 +0300

    add more test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """ client testter """
 
+from cgi import test
 import unittest
+from unittest import mock
+from defer import return_value
 from parameterized import parameterized
 from urllib.error import HTTPError
 from unittest.mock import patch, PropertyMock, Mock
@@ -17,7 +20,19 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch("client.get_json", return_value={"payload": True})
     def test_org(self, org_name, mock_get):
         """ Test GithubOrgClient.org and return """
-        test_client = GithubOrgClient(org_name)
-        test_return = test_client.org
+        test_return = GithubOrgClient(org_name).org
         self.assertEqual(test_return, mock_get.return_value)
         mock_get.assert_called_once
+
+    @patch('GithubOrgClient', return_value={'payload': True})
+    def test_public_repos_url(self):
+        """ Test if _public_repos_url result
+        is as expected """
+        with patch.object(GithubOrgClient, 'org', new_callable=PropertyMock,
+                          return_value={'repos_url': 'Belayneh'}) as mock_get:
+            test_json = {"repos_url": "Belayneh"}
+            test_client = GithubOrgClient(test_json.get("repos_url"))
+            test_return = test_client._public_repos_url
+            mock_get.assert_called_once
+            self.assertEqual(test_return,
+                             mock_get.return_value.get('repos_url'))
